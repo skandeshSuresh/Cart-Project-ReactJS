1886c8c11ae7468d8f81def9f54856125bce60d0
Skandesh Suresh <skandeshsuresh@gmail.com>
2021-12-17 10:08:32 +0530

    add increaseQuantity func

--- src/CartItem.js
+++ src/CartItem.js
@@ -3,18 +3,49 @@ import React from 'react';
 
 class CartItem extends React.Component {
 
+    constructor(){
+        super();
+        this.state = {
+            price: 999,
+            title: 'phone',
+            qty: 1,
+            img: ''
+        }
+        // this.increaseQuantity=this.increaseQuantity.bind(this);
+    }
+
+    increaseQuantity = () => {
+        
+
+    }
+
     render(){
+        const{price, title, qty} = this.state;
         return(
             <div className='cart-item'>
                 <div className='left-block'>
                     <img style={styles.image}/>
                 </div>
                 <div className='right-block'>
-                    <div style={{ fontSize: 25 }}>Phone</div>
-                    <div style={{ color: 'red' }}>Rs 999</div>
-                    <div style={{ color: 'blue'  }}>Qty: 1</div>
+                    <div style={{ fontSize: 25 }}>{this.state.title}</div>
+                    <div style={{ color: 'red' }}>Rs {price}</div>
+                    <div style={{ color: 'blue'  }}>Qty: {qty}</div>
                     <div className='cart-item-actions'>
                         {/*buttons*/}
+                        <img alt='increase' 
+                            className='action-icons' 
+                            src='https://cdn-icons-png.flaticon.com/512/992/992683.png' 
+                            // onClick={this.increaseQuantity.bind(this)}
+                            onClick={this.increaseQuantity}
+                        />
+                        <img alt='decrease' 
+                            className='action-icons' 
+                            src='https://cdn-icons.flaticon.com/png/512/3303/premium/3303893.png?token=exp=1639657987~hmac=a5fdb503e7833b27c0a15508eb9e7f3a' 
+                        />
+                        <img alt='delete' 
+                            className='action-icons' 
+                            src='https://cdn-icons.flaticon.com/png/512/484/premium/484611.png?token=exp=1639658030~hmac=792fd6866bbd99a4301d90cad69a9436' 
+                        />
                     </div>
                 </div>
 
