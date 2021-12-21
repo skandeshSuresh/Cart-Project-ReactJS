517f44e8668a25c8bbb05ae9d2980cc570a69a2f
Skandesh Suresh <skandeshsuresh@gmail.com>
2021-12-21 17:20:20 +0530

    add firebase config for the project

--- src/index.js
+++ src/index.js
@@ -2,6 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { initializeApp } from "firebase/app";
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCNf3oiGZuSrFjg14rTDehcTlxvGT2LZv4",
+  authDomain: "cart-90e0a.firebaseapp.com",
+  projectId: "cart-90e0a",
+  storageBucket: "cart-90e0a.appspot.com",
+  messagingSenderId: "129401502983",
+  appId: "1:129401502983:web:a6c7739ede6940dc9759a2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
